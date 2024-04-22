4cceeb46efa3a3d233038a2a227ec53746a8d64f
SMARAPD - Bruno Empke <BEMPKE@SMARAPD.COM.BR>
2024-04-22 13:04:57 -0300

    Versão Mobile

---
+++ main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // If we need pagination
+    slidesPerView: 3,
+    spaceBetween: 12,
+    pagination: {
+      el: '.swiper-pagination',
+      type:'bullets'
+    },
+  });
